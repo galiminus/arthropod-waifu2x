2866e69732d821661fb102be978d1fb6e1e9edae
Victor Goya <goya.victor@gmail.com>
2019-12-13 12:09:52 +0100

    fix fog arguments

--- lib/arthropod_waifu2x/scaler.rb
+++ lib/arthropod_waifu2x/scaler.rb
@@ -66,8 +66,8 @@ module ArthropodWaifu2x
     def storage
       @storage ||= Fog::Storage.new({
         provider:              'AWS',
-        access_key_id:         access_key_id,
-        secret_access_key:     secret_access_key,
+        aws_access_key_id:     access_key_id,
+        aws_secret_access_key: secret_access_key,
         region:                region,
       })
       @storage.directories.get(bucket)
